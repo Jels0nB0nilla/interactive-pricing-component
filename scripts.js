97cb1a944dd89b8803d896f9c21e24ff11edb4de
Jels0n B0nilla <jels0nb0nilla@gmail.com>
2024-05-05 17:03:52 -0400

    Updated html file

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const toggleBtn = document.querySelector('#toggle-btn');
 const startBtn = document.querySelector('.trial-btn');
 const mobileOff = document.querySelector('#mobile-off');
 
+// view, price, and discount Arrays
 const views = ['10k', '50k', '100k', '500k', '1M'];
 const prices = ['8', '12', '16', '24', '36'];
 const appliedDiscount = ['6', '9', '12', '18', '27'];
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   displayValues();
 });
 
-// Slider Event Listener
+// Slider 
 slider.addEventListener('input', () => {
   displayValues();
 });
@@ -30,13 +31,14 @@ const displayValues = () => {
   }
 };
 
-// Monthly / Yearly Toggle Event Listener
+// Monthly / Yearly Toggle
 discountToggle.addEventListener('click', () => {
   discountToggle.classList.toggle('yearly');
   // below - updates when toggle removes yearly
   displayValues();
 });
 
+// Updates button text 
 startBtn.addEventListener('click', () => {
   startBtn.innerText = 'Trial Begun!';
   startBtn.style.color = 'hsel(174, 77%, 80%)';
